12d07daf22a8dcb82a6ecb0af795f62215f9b360
gretamas <gmasliukova@gmail.com>
2024-05-11 20:39:14 +0300

    Add landing page

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+    $(".slider-2").slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+      variableWidth: true
+    });
+});
